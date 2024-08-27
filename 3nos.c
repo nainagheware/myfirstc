1383ba4c1aa275221a9072535475f50fb71d4d33
nainagheware <161754016+nainagheware@users.noreply.github.com>
2024-08-27 12:32:40 +0000

    update

--- 3nos.c
+++ 3nos.c
@@ -4,5 +4,5 @@ void main ()
     int a,b,c;
     printf("enter 3nos");
     scanf("%d%d%d",&a,&b,&c);
-    a>b && a>c ? printf("a is larger") : (b>c && b>a? : printf("b is larger:") : (c>a && c>b? : printf("c is larger") : (a==b && a==c : printf("they are equal") : printf("error"))));
+    a>b && a>c ? printf("a is larger") : ((b>c && b>a? : printf("b is larger:") ): (c>a && c>b? : printf("c is larger") : (a==b && a==c : printf("they are equal") : printf("error"))));
 }
