f39712310d6155b278c4ceca69719fc4ca616560
nainagheware <161754016+nainagheware@users.noreply.github.com>
2024-09-09 09:12:18 +0000

    update

--- kalakumbhchoice.c
+++ kalakumbhchoice.c
@@ -2,19 +2,22 @@
 void main ()
 {
     char name [20];
-    int age;
-    int class;
+    int age,class;
     char sec;
     char choice [3];
     printf("Enter your name: \n");
     scanf("%s", &name );
+    printf("Your name:%s\n",name);
     printf("enter your class:\n");
-    scanf("%d ", &class );
+    scanf("%d", &class );
+    printf("Your class:%d\n",class);
     printf("enter your sec:\n");
     scanf("%c", &sec );
+    printf("Your sec:%c\n",sec);
     printf("enter your age:\n");
     scanf("%d", &age );
+    printf("Your age:%d\n",age);
     printf("Would you like to participate in kalakumbh? \n");
     scanf("%s",&choice);
-    printf("name," of class",class,sec,"has chosen,"choice,", to participate in kalakumbh.);
+   printf("Your choice:%s\n",choice);
 }
