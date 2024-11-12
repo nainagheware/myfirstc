69dd94a601591b6214d7428e05339bf44102a8bb
nainagheware <161754016+nainagheware@users.noreply.github.com>
2024-11-12 15:52:14 +0000

    update

--- ascendingo.c
+++ ascendingo.c
@@ -7,23 +7,23 @@ scanf("%d%d%d",&a,&b,&c);
 if (a>b && a>c)
 {
     if(b>c)
-        printf("&d",c"<&d",b"<&d",a);
+        printf("&d",&c"<&d",&b"<&d",&a);
     else
-        printf("&d",b"<&d",c"<&d",a);
+        printf("&d",&b"<&d",&c"<&d",&a);
 }
 else if (b>c && b>a)
 {
     if(a>c)
-        printf("&d",c"<&d"a"<&d",b);
+        printf("&d",&c"<&d",&a"<&d",&b);
     else
-        printf("&d",a"<&d",c"<&d",b);
+        printf("&d",&a"<&d",&c"<&d",&b);
 }
 else if (c>b && c>a)
 {
     if(b>a)
-        printf("&d",a"<&d",b"<&d",c);
+        printf("&d",&a"<&d",&b"<&d",&c);
     else
-        printf("&d",b"<&d",a"<&d",c);
+        printf("&d",&b"<&d",&a"<&d",&c);
 }
 else
     printf("error");
