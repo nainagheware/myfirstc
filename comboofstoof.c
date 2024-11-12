1249f53eaf8039252e087331ca72f96ed64d728e
nainagheware <161754016+nainagheware@users.noreply.github.com>
2024-11-12 15:31:32 +0000

    update

--- comboofstoof.c
+++ comboofstoof.c
@@ -4,7 +4,7 @@ void main()
 char c;
 printf("enter a alphabet/num/special charachter");
 scanf("%c",&c);
-if (c>=65 && c<=90)
+if (c>=65 && c<=90 || c>=97 && c<=122)
 {
     if(c=='a' ||c=='e' ||c=='i'||c=='o'|| c=='u')
         printf("it is a vowel");
